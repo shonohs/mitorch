e0b56f1666f7e10bdca98d019783837b2f891ef2
shono <>
2020-07-16 22:08:26 -0700

    Add Lr Warmup.

--- mitorch/builders/lr_scheduler_builder.py
+++ mitorch/builders/lr_scheduler_builder.py
@@ -11,6 +11,26 @@ class LinearDecreasingLR(torch.optim.lr_scheduler.LambdaLR):
         super().__init__(optimizer, self.lr_lambda, last_iter)
 
 
+class WarmupLR(torch.optim.lr_scheduler.LambdaLR):
+    def lr_lambda(self, iteration):
+        return self.warmup_factor if iteration <= self.warmup_iters else 1
+
+    def __init__(self, lr_scheduler, warmup_iters, warmup_factor, last_iter=-1):
+        self.warmup_iters = warmup_iters
+        self.warmup_factor = warmup_factor
+        self.lr_scheduler = lr_scheduler
+        super().__init__(lr_scheduler.optimizer, self.lr_lambda, lr_scheduler.last_epoch)
+
+    def step(self, iteration=None):
+        # In warmup period, call the original lr_scheduler first, then overwrite the lr with warmup LR.
+        if self.last_epoch < self.warmup_iters:
+            self.lr_scheduler.step(iteration)
+            super().step(iteration)
+        else:
+            super().step(iteration)
+            self.lr_scheduler.step(iteration)
+
+
 class LrSchedulerBuilder:
     def __init__(self, config):
         self.config = config['lr_scheduler']
@@ -18,8 +38,16 @@ class LrSchedulerBuilder:
     def build(self, optimizer, total_iters):
         logging.info(f"Building a lr_scheduler. total_iters: {total_iters}, config: {self.config}")
         if self.config['name'] == 'cosine_annealing':
-            return torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, total_iters)
+            lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, total_iters)
         elif self.config['name'] == 'linear_decreasing':
-            return LinearDecreasingLR(optimizer, total_iters)
+            lr_scheduler = LinearDecreasingLR(optimizer, total_iters)
         else:
             raise NotImplementedError(f"Unsupported LR scheduler: {self.config['name']}")
+
+        warmup_iters = self.config.get('warmup_iters', 0)
+        if warmup_iters > 0:
+            warmup_factor = self.config.get('warmup_factor', 0.01)
+            lr_scheduler = WarmupLR(lr_scheduler, warmup_iters, warmup_factor)
+            logging.info(f"Using Lr Warmup: {warmup_iters} iters with {warmup_factor}")
+
+        return lr_scheduler
--- test/test_lr_scheduler_builder.py
+++ test/test_lr_scheduler_builder.py
@@ -1,7 +1,7 @@
 import pickle
 import unittest
 import torch
-from mitorch.builders.lr_scheduler_builder import LinearDecreasingLR
+from mitorch.builders.lr_scheduler_builder import LinearDecreasingLR, WarmupLR
 
 
 class TestLrScheduler(unittest.TestCase):
@@ -14,6 +14,26 @@ class TestLrScheduler(unittest.TestCase):
         new_scheduler = pickle.loads(pickle.dumps(scheduler))
         new_result = new_scheduler.step(10)
         self.assertEqual(result, new_result)
+
+    def test_warmup_lr(self):
+        parameters = torch.nn.Conv2d(1, 1, 1).parameters()
+        optimizer = torch.optim.SGD(parameters, lr=1)
+        scheduler = LinearDecreasingLR(optimizer, 100)
+        warmup_scheduler = WarmupLR(scheduler, 5, 0.01)
+
+        lrs = self.step_and_get_lr(warmup_scheduler, optimizer, 10)
+        expected_lrs = [0.01, 0.01, 0.01, 0.01, 0.01, 0.94, 0.93, 0.92, 0.91, 0.90]
+        for lr, expected in zip(lrs, expected_lrs):
+            self.assertAlmostEqual(lr, expected)
+
+    @staticmethod
+    def step_and_get_lr(scheduler, optimizer, num_steps):
+        lrs = []
+        for i in range(num_steps):
+            optimizer.step()
+            lrs.append(optimizer.param_groups[0]['lr'])
+            scheduler.step()
+        return lrs
 
 
 if __name__ == '__main__':
